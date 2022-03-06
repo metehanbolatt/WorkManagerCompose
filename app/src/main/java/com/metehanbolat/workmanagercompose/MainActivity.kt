af982384d9ee6755855e67ccf52d8a4fbdfeb544
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-03-07 01:43:38 +0300

    WorkManager.beginUniqueWork - WorkInfo.State Control - UI

--- app/src/main/java/com/metehanbolat/workmanagercompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/workmanagercompose/MainActivity.kt
@@ -3,12 +3,20 @@ package com.metehanbolat.workmanagercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.work.*
+import coil.compose.rememberImagePainter
 import com.metehanbolat.workmanagercompose.ui.theme.WorkManagerComposeTheme
 import java.time.Duration
 
@@ -49,6 +57,59 @@ class MainActivity : ComponentActivity() {
                     val downloadUri = downloadInfo?.outputData?.getString(WorkerKeys.IMAGE_URI)?.toUri()
                     val filterUri = filterInfo?.outputData?.getString(WorkerKeys.FILTER_URI)?.toUri()
                     filterUri ?: downloadUri
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    imageUri?.let { uri ->
+                        Image(
+                            painter = rememberImagePainter(
+                                data = uri
+                            ),
+                            contentDescription = "Uri Image",
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
+                    Button(
+                        onClick = {
+                            workManager
+                                .beginUniqueWork(
+                                    "download",
+                                    ExistingWorkPolicy.KEEP,
+                                    downloadRequest
+                                )
+                                .then(colorFilterRequest)
+                                .enqueue()
+                        },
+                        enabled = downloadInfo?.state != WorkInfo.State.RUNNING
+                    ) {
+                        Text(text = "Start Download")
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    when(downloadInfo?.state) {
+                        WorkInfo.State.RUNNING -> Text(text = "Downloading...")
+                        WorkInfo.State.SUCCEEDED -> Text(text = "Download Succeeded")
+                        WorkInfo.State.FAILED -> Text(text = "Download Failed...")
+                        WorkInfo.State.CANCELLED -> Text(text = "Download Cancelled...")
+                        WorkInfo.State.ENQUEUED -> Text(text = "Download Enqueued...")
+                        WorkInfo.State.BLOCKED -> Text(text = "Download Blocked...")
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    when(filterInfo?.state) {
+                        WorkInfo.State.RUNNING -> Text(text = "Applying Filter...")
+                        WorkInfo.State.SUCCEEDED -> Text(text = "Filter Succeeded")
+                        WorkInfo.State.FAILED -> Text(text = "Filter Failed...")
+                        WorkInfo.State.CANCELLED -> Text(text = "Filter Cancelled...")
+                        WorkInfo.State.ENQUEUED -> Text(text = "Filter Enqueued...")
+                        WorkInfo.State.BLOCKED -> Text(text = "Filter Blocked...")
+                    }
                 }
 
             }
